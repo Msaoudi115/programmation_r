fc1a5655a775779fc8886ebdff6c72876d41a7cf
Msaoudi115 <123741565+Msaoudi115@users.noreply.github.com>
2023-03-21 15:20:23 +0100

    Update TD 6.R

--- TD 6/TD 6.R
+++ TD 6/TD 6.R
@@ -16,13 +16,34 @@ summary(df)
 
 #1.e
 for ( i  in colnames(df)) 
-{ if ( class(df[,i]) !="numeric" )
+{ if ( is.numeric(df[,i])==F  )
  { df[,i]<- as.factor(df[,i])}
 }
 
 #1.f
-
 str(df)
+
 
 #2.a
 length(unique(df$brands))
+
+
+#2.b
+
+tri_marque = table(df$brands)
+
+barplot(tri_marque, 
+        main = "répartition du nombre de resto par marque",
+        col = colors()[c(45,99)], horiz = FALSE)
+
+#2.c
+
+median(df$energy.kcal_value, na.rm = T)
+sd(df$energy.kcal_value, na.rm = T)
+
+#2.d
+boxplot(df$energy.kcal_value, na.rm = T,main = "boite à moustache")
+
+#2.e
+
+rang1 = order(df$energy.kcal_value, )
