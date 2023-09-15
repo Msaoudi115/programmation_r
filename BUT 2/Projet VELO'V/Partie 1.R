3f1645b9cb3f9c545a4098f27c95239a3bf1e436
Msaoudi115 <123741565+Msaoudi115@users.noreply.github.com>
2023-09-15 16:26:59 +0200

    Update

--- BUT 2/Projet VELO'V/Partie 1.R
+++ BUT 2/Projet VELO'V/Partie 1.R
@@ -1,3 +1,8 @@
+install.packages("httr")
+install.packages("fromjson")
+library("httr")
+library("jsonlite")
+
 apiKey = "88513cafaa4c5466b2a0e37324cdb9b1d3c73d8c"
 contract_name = "Lyon"
 api_url <- paste0("https://api.jcdecaux.com/vls/v1/stations?contract=", contract_name, "&apiKey=", apiKey)
@@ -6,3 +11,15 @@ api_url <- "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=88513c
 raw_dataaa <- GET(api_url)
 DLyon <- fromJSON(rawToChar(raw_dataaa$content), flatten = TRUE)
 
+# Tuto BDD R https://www.slideshare.net/RsquaredIn/rmysql-tutorial-for-beginners
+
+install.packages("RMySQL")
+library("RMySQL")
+
+con = dbConnect(MySQL(),
+                user = "sql11646697",
+                password = "3x2eA5a3nT",
+                host = "sql11.freesqldatabase.com",
+                dbname = "sql11646697")
+summary(con)
+dbListTables(con)
