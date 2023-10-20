0eb5a2df3126a96fcc631b89f5a51ffc3b136c5e
Msaoudi115 <123741565+Msaoudi115@users.noreply.github.com>
2023-10-20 11:48:21 +0200

    Update

--- BUT 2/Projet VELOV/Test App.R
+++ BUT 2/Projet VELOV/Test App.R
@@ -1,114 +1,82 @@
-# Define UI for random distribution app ----
+# Charger les bibliothèques requises
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Définir l'interface utilisateur (UI) pour l'application Shiny
 ui <- fluidPage(
   
-  # App title ----
   titlePanel("Tabsets"),
   
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
     sidebarPanel(
       
-      # Input: Select the random distribution type ----
       radioButtons("dist", "Distribution type:",
                    c("Normal" = "norm",
                      "Uniform" = "unif",
                      "Log-normal" = "lnorm",
                      "Exponential" = "exp")),
       
-      # br() element to introduce extra vertical spacing ----
       br(),
       
-      # Input: Slider for the number of observations to generate ----
       sliderInput("n",
                   "Number of classes:",
                   value = 10,
                   min = 1,
                   max = 20)
-      
     ),
     
-    # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
                   tabPanel("Plot", plotOutput("plot")),
                   tabPanel("Summary", verbatimTextOutput("summary")),
-                  tabPanel("Table", tableOutput("table"))
+                  tabPanel("Table", tableOutput("table")),
+                  tabPanel("Graph", plotOutput("graphique"))
       )
-      
     )
   )
 )
 
-# Define server logic for random distribution app ----
+# Définir la logique du serveur pour l'application Shiny
 server <- function(input, output) {
   data <- read.csv("C:/Users/msaoudi/Documents/GitHub/programmation_r/BUT 2/Projet VELOV/Excel.csv")
-  # Reactive expression to generate the requested distribution ----
-  # This is called whenever the inputs change. The output functions
-  # defined below then use the value computed from this expression
-  # Reactive expression to generate the requested distribution ----
+  
   d <- reactive({
- 
     data
   })
   
-  # Generate a plot of the data ----
-  # Also uses the inputs to build the plot label. Note that the
-  # dependencies on the inputs and the data reactive expression are
-  # both tracked, and all expressions are called in the sequence
-  # implied by the dependency graph.
   output$plot <- renderPlot({
-    data$available_bikes
-    
     hist(data$available_bikes,
          main = "histo",
          col = "#75AADB", border = "white", breaks = input$n)
   })
   
-  # Generate a summary of the data ----
   output$summary <- renderPrint({
     summary(d())
   })
   
-  # Generate an HTML table view of the data ----
   output$table <- renderTable({
     d()
   })
-  
-}
-
-# Create Shiny app ----
-shinyApp(ui, server)
-
-write.csv(DLyon, "C:/Users/msaoudi/Documents/GitHub/programmation_r/BUT 2/Projet VELOV/Excel.csv", row.names=TRUE)
-install.packages("leaflet")
-install.packages("dplyr")
-library(shiny)
-library(leaflet)
-library(dplyr)
-
-#Script de la carte des stations velo
-# Read the CSV data
-data <- read.csv("C:/Users/msaoudi/Documents/GitHub/programmation_r/BUT 2/Projet VELOV/Excel.csv")
-
-ui <- fluidPage(
-  titlePanel("Lyon City Map"),
-  leafletOutput("map")
-)
-
-server <- function(input, output, session) {
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      addMarkers(
-        data = data,
-        lat = ~`position.lat`,   # Use position.lat column
-        lng = ~`position.lng`    # Use position.lng column
-      )
+# Graphique Top 10 stations avec le plus de velo (fonctionne à ne pas modifier )  
+  output$graphique <- renderPlot({
+    data_filtered <- data %>%
+      arrange(desc(available_bikes)) %>%
+      head(10)
+    
+    data_filtered$name <- substr(data_filtered$name, 8, nchar(data_filtered$name)) # Enlever les 5 premiers caractères
+    
+    ggplot(data_filtered, aes(x = reorder(name, -available_bikes), y = available_bikes)) +
+      geom_bar(stat = "identity") +
+      labs(x = "Name", y = "Available bikes", title = "Stations with the most available bikes") +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
   })
 }
 
+# Créer l'application Shiny
 shinyApp(ui, server)
+
+
+write.csv(DLyon, "C:/Users/msaoudi/Documents/GitHub/programmation_r/BUT 2/Projet VELOV/Excel.csv", row.names=TRUE)
+
